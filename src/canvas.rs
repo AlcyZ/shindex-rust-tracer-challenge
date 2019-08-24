4cbdac04f9454cca27504864d4bab1e4cc288505
alcyz <tobias_schindler@hotmail.de>
2019-08-24 18:27:24 +0200

    colors and canvas implementation

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,41 +1,173 @@
-use crate::color::{Color, color};
+use crate::color::{col_to_string, Color};
 
-#[derive(Debug)]
 struct Canvas {
     width: usize,
     height: usize,
     pixels: Vec<Vec<Color>>,
 }
 
-fn canvas(width: usize, height: usize) -> Canvas {
-    let mut pixels: Vec<Vec<Color>> = Vec::new();
-    let black = color(0_f64, 0_f64, 0_f64);
-
-
-    for w in 0..width {
-        for h in 0..height {
-            let mut foo: Vec<Color> = Vec::new();
-            foo.push(black.clone());
-
-            pixels.push(foo)
-        }
+fn ppm_push(data: &mut Vec<String>, line: &mut String, col_str: String) {
+    if line.len() > 67 {
+        data.push(line[0..line.len() - 1].to_string());
+        line.truncate(0);
     }
-
-//    let r = vec![color(0_f64, 0_f64, 0_f64); width];
-//    let pixels = vec![r; height];
-
-    Canvas { width, height, pixels }
+    line.push_str(&col_str);
+    line.push_str(" ");
 }
 
-#[cfg(Test)]
+impl Canvas {
+    fn new(width: usize, height: usize) -> Canvas {
+        let r = vec![Color::black(); width];
+        let pixels = vec![r; height];
+
+        Canvas { width, height, pixels }
+    }
+
+    fn pixel_at(&self, width: usize, height: usize) -> &Color {
+        &self.pixels[height][width]
+    }
+
+    fn write_pixel(&mut self, width: usize, height: usize, c: Color) {
+        self.pixels[height][width] = c
+    }
+
+    fn ppm_header(&self) -> String {
+        format!("P3\n{} {}\n255", self.width, self.height)
+    }
+
+    fn ppm_body(&self) -> String {
+        let mut lines: Vec<String> = Vec::new();
+
+        for h in 0..self.height {
+            let mut line = "".to_string();
+            for w in 0..self.width {
+                let pix = self.pixel_at(w, h);
+                let red = col_to_string(&pix.red);
+                let green = col_to_string(&pix.green);
+                let blue = col_to_string(&pix.blue);
+
+
+                ppm_push(&mut lines, &mut line, red);
+                ppm_push(&mut lines, &mut line, green);
+                ppm_push(&mut lines, &mut line, blue);
+            }
+            lines.push(line[0..line.len() - 1].to_string())
+        }
+
+        lines.push("".to_string());
+        lines.join("\n")
+    }
+
+    fn to_ppm(&self) -> String {
+        let header = self.ppm_header();
+        let body = self.ppm_body();
+        let data = [header, body];
+
+        data.join("\n")
+    }
+}
+
+#[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::canvas::Canvas;
+    use crate::color::Color;
 
     #[test]
     fn creating_a_canvas() {
-        let c = canvas(10, 20);
+        let c = Canvas::new(10, 20);
+        let black = Color::new(0_f64, 0_f64, 0_f64);
 
-        assert_eq!(c.width, 10);
-        assert_eq!(c.height, 20);
+        assert_eq!(10, c.width);
+        assert_eq!(20, c.height);
+
+        for w in 0..c.width {
+            for h in 0..c.height {
+                assert_eq!(&black, c.pixel_at(w, h));
+            }
+        }
+    }
+
+    #[test]
+    fn writing_pixels_to_canvas() {
+        let mut c = Canvas::new(10, 20);
+        let red = Color::new(1_f64, 0_f64, 0_f64);
+
+        c.write_pixel(2, 3, red);
+        assert_eq!(&Color::new(1_f64, 0_f64, 0_f64), c.pixel_at(2, 3))
+    }
+
+    #[test]
+    fn constructing_ppm_header() {
+        let c = Canvas::new(5, 3);
+        let ppm = c.to_ppm();
+        let expected = ["P3", "5 3", "255"];
+
+        for (i, l) in ppm.lines().enumerate() {
+            if i < 3 {
+                assert_eq!(l, expected[i])
+            }
+        }
+    }
+
+    #[test]
+    fn constructing_ppm_pixel_data() {
+        let mut c = Canvas::new(5, 3);
+        let c1 = Color::new(1.5, 0_f64, 0_f64);
+        let c2 = Color::new(0_f64, 0.5, 0_f64);
+        let c3 = Color::new(-0.5, 0_f64, 1_f64);
+
+        c.write_pixel(0, 0, c1);
+        c.write_pixel(2, 1, c2);
+        c.write_pixel(4, 2, c3);
+
+        let ppm = c.to_ppm();
+        let expected = [
+            "P3",
+            "5 3",
+            "255",
+            "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+            "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0",
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"
+        ];
+
+        let mut asserted = false;
+        for (i, l) in ppm.lines().enumerate() {
+            if i >= 3 && i <= 5 {
+                assert_eq!(l, expected[i]);
+                asserted = true;
+            }
+        }
+        assert!(asserted)
+    }
+
+    #[test]
+    fn splitting_long_lines_in_ppm_files() {
+        let mut c = Canvas::new(10, 2);
+
+        for w in 0..c.width {
+            for h in 0..c.height {
+                c.write_pixel(w, h, Color::new(1_f64, 0.8, 0.6))
+            }
+        }
+
+        let expected = [
+            "P3",
+            "10 2",
+            "255",
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204",
+            "153 255 204 153 255 204 153 255 204 153 255 204 153",
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204",
+            "153 255 204 153 255 204 153 255 204 153 255 204 153"
+        ];
+        let ppm = c.to_ppm();
+
+        let mut asserted = false;
+        for (i, l) in ppm.lines().enumerate() {
+            if i >= 3 && i <= 6 {
+                assert_eq!(l, expected[i]);
+                asserted = true;
+            }
+        }
+        assert!(asserted)
     }
 }
--- src/color.rs
+++ src/color.rs
@@ -1,8 +1,8 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Color {
-    red: f64,
-    green: f64,
-    blue: f64,
+    pub(crate) red: f64,
+    pub(crate) green: f64,
+    pub(crate) blue: f64,
 }
 
 impl std::cmp::PartialEq for Color {
@@ -59,8 +59,33 @@ impl std::ops::Mul<f64> for Color {
     }
 }
 
-pub fn color(red: f64, green: f64, blue: f64) -> Color {
-    Color { red, green, blue }
+pub fn col_to_string(color: &f64) -> String {
+    let col: f64;
+    if *color > 1_f64 {
+        col = 1_f64
+    } else if *color < 0_f64 {
+        col = 0_f64
+    } else {
+        col = *color
+    }
+
+    (col * 255__f64).round().to_string()
+}
+
+impl Color {
+    pub fn new(red: f64, green: f64, blue: f64) -> Color {
+        Color { red, green, blue }
+    }
+
+    pub fn black() -> Color {
+        Color { red: 0_f64, green: 0_f64, blue: 0_f64 }
+    }
+
+    pub fn to_ppm_data(&self) -> String {
+        let data = [col_to_string(&self.red), col_to_string(&self.green), col_to_string(&self.blue)];
+
+        data.join(" ")
+    }
 }
 
 #[cfg(test)]
@@ -69,7 +94,7 @@ mod tests {
 
     #[test]
     fn color_are_red_green_blue() {
-        let c = color(1_f64, 2_f64, 3_f64);
+        let c = Color::new(1_f64, 2_f64, 3_f64);
 
         assert_eq!(c.red, 1_f64);
         assert_eq!(c.green, 2_f64);
@@ -78,35 +103,35 @@ mod tests {
 
     #[test]
     fn adding_colors() {
-        let a = color(1_f64, 2_f64, 3_f64);
-        let b = color(2_f64, 3_f64, 4_f64);
-        let c = color(3_f64, 5_f64, 7_f64);
+        let a = Color::new(1_f64, 2_f64, 3_f64);
+        let b = Color::new(2_f64, 3_f64, 4_f64);
+        let c = Color::new(3_f64, 5_f64, 7_f64);
 
         assert_eq!(c, a + b)
     }
 
     #[test]
     fn subtracting_colors() {
-        let a = color(1_f64, 2_f64, 3_f64);
-        let b = color(2_f64, 3_f64, -4_f64);
-        let c = color(-1_f64, -1_f64, 7_f64);
+        let a = Color::new(1_f64, 2_f64, 3_f64);
+        let b = Color::new(2_f64, 3_f64, -4_f64);
+        let c = Color::new(-1_f64, -1_f64, 7_f64);
 
         assert_eq!(c, a - b)
     }
 
     #[test]
     fn multiply_color_and_scalar() {
-        let a = color(1_f64, 2_f64, 3_f64);
-        let b = color(3.5, 7_f64, 10.5);
+        let a = Color::new(1_f64, 2_f64, 3_f64);
+        let b = Color::new(3.5, 7_f64, 10.5);
 
         assert_eq!(b, a * 3.5)
     }
 
     #[test]
     fn multiply_colors() {
-        let a = color(1_f64, 2_f64, 3_f64);
-        let b = color(2_f64, 3_f64, 4_f64);
-        let c = color(2_f64, 6_f64, 12_f64);
+        let a = Color::new(1_f64, 2_f64, 3_f64);
+        let b = Color::new(2_f64, 3_f64, 4_f64);
+        let c = Color::new(2_f64, 6_f64, 12_f64);
 
         assert_eq!(c, a * b)
     }
