9564e3e1f4fbc56426cac09e90834d689fa7c406
Tobias Schindler <t.schindler@gambio.de>
2019-08-26 18:39:21 +0200

    removed all abstractions from tuples

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io::Write;
 
 use crate::canvas::Canvas;
 use crate::color::Color;
-use crate::tuple::{normalize, point, Tuple, vector};
+use crate::tuple::{normalize, point, Tuple, vector, tuple_add, tuple_mul_scalar};
 use std::time::Instant;
 
 mod tuple;
@@ -23,12 +23,12 @@ struct Environment {
 fn tick(env: &Environment, pro: Projectile, canvas: &mut Canvas) -> Projectile {
     let old_vel = pro.velocity;
 
-    let position = pro.position + old_vel;
-    let velocity = old_vel + env.gravity + env.wind;
+    let position = tuple_add(pro.position, old_vel);
+    let velocity = tuple_add(tuple_add(old_vel, env.gravity), env.wind);
 
     let color = Color::new(0__f64, 1_f64, 0_f64);
-    let w = pro.position.x as usize;
-    let h = canvas.height - pro.position.y as usize;
+    let w = pro.position[0] as usize;
+    let h = canvas.height - pro.position[1] as usize;
 
     canvas.write_pixel(w, h, color);
 
@@ -39,9 +39,11 @@ fn main() {
     let now = Instant::now();
 
     let start = point(0_f64, 1_f64, 0_f64);
+
     let mut velocity = vector(1_f64, 1.8, 0_f64);
     normalize(&mut velocity);
-    velocity = velocity * 11.25;
+    velocity = tuple_mul_scalar(velocity, 11.25);
+
     let mut projectile = Projectile { position: start, velocity };
 
     let gravity = vector(0_f64, -0.1, 0_f64);
@@ -51,7 +53,7 @@ fn main() {
     let mut c = Canvas::new(900, 550);
 
 
-    while projectile.position.y > 0 as f64 {
+    while projectile.position[1] > 0 as f64 {
         projectile = tick(&env, projectile, &mut c);
     }
 
